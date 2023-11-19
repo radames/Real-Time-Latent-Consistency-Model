918ded46b86ea6cdc664ac11bb33ae634d4f1751
Radamés Ajna <radamajna@gmail.com>
2023-11-18 16:16:27 -0800

    use proxy to run dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '^/settings|/queue_size|/stream': 'http://localhost:7860',
+      '/ws': {
+        target: 'ws://localhost:7860',
+        ws: true
+      }
+    },
+  }
 });
