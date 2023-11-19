0835cd80607e0eb72879c3c3c75a727d636f0fd2
Radamés Ajna <radamajna@gmail.com>
2023-11-18 16:18:08 -0800

    add snapshot button

--- frontend/src/lib/components/ImagePlayer.svelte
+++ frontend/src/lib/components/ImagePlayer.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
   import { lcmLiveStatus, LCMLiveStatus, streamId } from '$lib/lcmLive';
-  import { onFrameChangeStore } from '$lib/mediaStream';
-  import { PUBLIC_BASE_URL } from '$env/static/public';
+  import Button from '$lib/components/Button.svelte';
+  import { snapImage } from '$lib/utils';
 
   $: {
     console.log('streamId', $streamId);
   }
   $: isLCMRunning = $lcmLiveStatus !== LCMLiveStatus.DISCONNECTED;
   $: console.log('isLCMRunning', isLCMRunning);
+  let imageEl: HTMLImageElement;
+  async function takeSnapshot() {
+    if (isLCMRunning) {
+      await snapImage(imageEl);
+    }
+  }
 </script>
 
-<div class="relative overflow-hidden rounded-lg border border-slate-300">
-  <!-- svelte-ignore a11y-missing-attribute -->
-  {#if isLCMRunning}
-    <img class="aspect-square w-full rounded-lg" src={PUBLIC_BASE_URL + '/stream/' + $streamId} />
-  {:else}
-    <div class="aspect-square w-full rounded-lg" />
-  {/if}
-  <div class="absolute left-0 top-0 aspect-square w-1/4">
-    <slot />
+<div class="flex flex-col">
+  <div class="relative overflow-hidden rounded-lg border border-slate-300">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    {#if isLCMRunning}
+      <img
+        bind:this={imageEl}
+        class="aspect-square w-full rounded-lg"
+        src={'/stream/' + $streamId}
+      />
+    {:else}
+      <div class="aspect-square w-full rounded-lg" />
+    {/if}
+    <div class="absolute left-0 top-0 aspect-square w-1/4">
+      <slot />
+    </div>
   </div>
+  <Button on:click={takeSnapshot} disabled={!isLCMRunning} classList={'ml-auto'}>Snapshot</Button>
 </div>
