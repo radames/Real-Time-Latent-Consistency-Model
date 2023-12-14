312081c9069f3e804a589982be47b0e9cc08dc16
designresearchworks <94980251+designresearchworks@users.noreply.github.com>
2023-12-14 19:10:37 +0000

    adding npm script

---
+++ install_npm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Download and install NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install the latest version of Node.js using NVM
+nvm install node
