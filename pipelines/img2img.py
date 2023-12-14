078aefd7228fe19c3f1ae7256da03399cfff3467
designresearchworks <94980251+designresearchworks@users.noreply.github.com>
2023-12-14 19:40:15 +0000

    updating npm build command

--- pipelines/img2img.py
+++ pipelines/img2img.py
@@ -42,10 +42,10 @@ class Pipeline:
             4, min=2, max=15, title="Steps", field="range", hide=True, id="steps"
         )
         width: int = Field(
-            100, min=2, max=15, title="Width", disabled=True, hide=True, id="width"
+            512, min=2, max=15, title="Width", disabled=True, hide=True, id="width"
         )
         height: int = Field(
-            100, min=2, max=15, title="Height", disabled=True, hide=True, id="height"
+            512, min=2, max=15, title="Height", disabled=True, hide=True, id="height"
         )
         guidance_scale: float = Field(
             0.2,
