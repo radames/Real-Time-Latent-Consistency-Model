ee78277bc90fd4eb16378f1094e962fc5b73f118
Radamés Ajna <radamajna@gmail.com>
2023-12-15 22:49:28 -0800

    add img2imgSegmindVegaRT

---
+++ pipelines/img2imgSegmindVegaRT.py
@@ -0,0 +1,209 @@
+from diffusers import (
+    AutoPipelineForImage2Image,
+    LCMScheduler,
+    AutoencoderTiny,
+)
+from compel import Compel, ReturnedEmbeddingsType
+import torch
+
+try:
+    import intel_extension_for_pytorch as ipex  # type: ignore
+except:
+    pass
+
+import psutil
+from config import Args
+from pydantic import BaseModel, Field
+from PIL import Image
+import math
+
+base_model = "segmind/Segmind-Vega"
+lora_model = "segmind/Segmind-VegaRT"
+taesd_model = "madebyollin/taesdxl"
+
+default_prompt = "close-up photography of old man standing in the rain at night, in a street lit by lamps, leica 35mm summilux"
+default_negative_prompt = "blurry, low quality, render, 3D, oversaturated"
+page_content = """
+<h1 class="text-3xl font-bold">Real-Time SDXL Turbo</h1>
+<h3 class="text-xl font-bold">Image-to-Image</h3>
+<p class="text-sm">
+    This demo showcases
+    <a
+    href="https://huggingface.co/stabilityai/sdxl-turbo"
+    target="_blank"
+    class="text-blue-500 underline hover:no-underline">SDXL Turbo</a>
+Image to Image pipeline using
+    <a
+    href="https://huggingface.co/docs/diffusers/main/en/using-diffusers/sdxl_turbo"
+    target="_blank"
+    class="text-blue-500 underline hover:no-underline">Diffusers</a
+    > with a MJPEG stream server.
+</p>
+<p class="text-sm text-gray-500">
+    Change the prompt to generate different images, accepts <a
+    href="https://github.com/damian0815/compel/blob/main/doc/syntax.md"
+    target="_blank"
+    class="text-blue-500 underline hover:no-underline">Compel</a
+    > syntax.
+</p>
+"""
+
+
+class Pipeline:
+    class Info(BaseModel):
+        name: str = "img2img"
+        title: str = "Image-to-Image Playground 256"
+        description: str = "Generates an image from a text prompt"
+        input_mode: str = "image"
+        page_content: str = page_content
+
+    class InputParams(BaseModel):
+        prompt: str = Field(
+            default_prompt,
+            title="Prompt",
+            field="textarea",
+            id="prompt",
+        )
+        negative_prompt: str = Field(
+            default_negative_prompt,
+            title="Negative Prompt",
+            field="textarea",
+            id="negative_prompt",
+            hide=True,
+        )
+        seed: int = Field(
+            2159232, min=0, title="Seed", field="seed", hide=True, id="seed"
+        )
+        steps: int = Field(
+            4, min=1, max=15, title="Steps", field="range", hide=True, id="steps"
+        )
+        width: int = Field(
+            1024, min=2, max=15, title="Width", disabled=True, hide=True, id="width"
+        )
+        height: int = Field(
+            1024, min=2, max=15, title="Height", disabled=True, hide=True, id="height"
+        )
+        guidance_scale: float = Field(
+            0.2,
+            min=0,
+            max=20,
+            step=0.001,
+            title="Guidance Scale",
+            field="range",
+            hide=True,
+            id="guidance_scale",
+        )
+        strength: float = Field(
+            0.5,
+            min=0.25,
+            max=1.0,
+            step=0.001,
+            title="Strength",
+            field="range",
+            hide=True,
+            id="strength",
+        )
+
+    def __init__(self, args: Args, device: torch.device, torch_dtype: torch.dtype):
+        if args.safety_checker:
+            self.pipe = AutoPipelineForImage2Image.from_pretrained(
+                base_model,
+                variant="fp16",
+            )
+        else:
+            self.pipe = AutoPipelineForImage2Image.from_pretrained(
+                base_model,
+                safety_checker=None,
+                variant="fp16",
+            )
+        if args.use_taesd:
+            self.pipe.vae = AutoencoderTiny.from_pretrained(
+                taesd_model, torch_dtype=torch_dtype, use_safetensors=True
+            ).to(device)
+
+        self.pipe.load_lora_weights(lora_model)
+        self.pipe.fuse_lora()
+        self.pipe.scheduler = LCMScheduler.from_pretrained(
+            base_model, subfolder="scheduler"
+        )
+        self.pipe.set_progress_bar_config(disable=True)
+        self.pipe.to(device=device, dtype=torch_dtype)
+        if device.type != "mps":
+            self.pipe.unet.to(memory_format=torch.channels_last)
+
+        # check if computer has less than 64GB of RAM using sys or os
+        if psutil.virtual_memory().total < 64 * 1024**3:
+            self.pipe.enable_attention_slicing()
+
+        if args.torch_compile:
+            print("Running torch compile")
+            self.pipe.unet = torch.compile(
+                self.pipe.unet,
+            )
+            self.pipe.vae = torch.compile(
+                self.pipe.vae,
+            )
+
+            self.pipe(
+                prompt="warmup",
+                image=[Image.new("RGB", (768, 768))],
+            )
+        if args.compel:
+            self.pipe.compel_proc = Compel(
+                tokenizer=[self.pipe.tokenizer, self.pipe.tokenizer_2],
+                text_encoder=[self.pipe.text_encoder, self.pipe.text_encoder_2],
+                returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,
+                requires_pooled=[False, True],
+            )
+
+    def predict(self, params: "Pipeline.InputParams") -> Image.Image:
+        generator = torch.manual_seed(params.seed)
+        prompt = params.prompt
+        negative_prompt = params.negative_prompt
+        prompt_embeds = None
+        pooled_prompt_embeds = None
+        negative_prompt_embeds = None
+        negative_pooled_prompt_embeds = None
+        if hasattr(self.pipe, "compel_proc"):
+            prompt_embeds = self.pipe.compel_proc(
+                [params.prompt, params.negative_prompt]
+            )
+            prompt = None
+            negative_prompt = None
+            prompt_embeds = prompt_embeds[0:1]
+            pooled_prompt_embeds = pooled_prompt_embeds[0:1]
+            negative_prompt_embeds = prompt_embeds[1:2]
+            negative_pooled_prompt_embeds = pooled_prompt_embeds[1:2]
+
+        steps = params.steps
+        strength = params.strength
+        if int(steps * strength) < 1:
+            steps = math.ceil(1 / max(0.10, strength))
+
+        results = self.pipe(
+            image=params.image,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            prompt_embeds=prompt_embeds,
+            pooled_prompt_embeds=pooled_prompt_embeds,
+            negative_prompt_embeds=negative_prompt_embeds,
+            negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+            generator=generator,
+            strength=strength,
+            num_inference_steps=steps,
+            guidance_scale=params.guidance_scale,
+            width=params.width,
+            height=params.height,
+            output_type="pil",
+        )
+
+        nsfw_content_detected = (
+            results.nsfw_content_detected[0]
+            if "nsfw_content_detected" in results
+            else False
+        )
+        if nsfw_content_detected:
+            return None
+        result_image = results.images[0]
+
+        return result_image
