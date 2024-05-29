a83681b78ce118e4a2107954811b800d9c41c5c8
Radamés Ajna <radamajna@gmail.com>
2024-05-28 18:05:13 -0700

    bump

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess({ postcss: true }),
