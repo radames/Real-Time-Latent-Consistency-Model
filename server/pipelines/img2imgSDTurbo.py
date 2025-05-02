409869d58f7740e4a4ff903dc47d959caa5b863b
Radamés Ajna <radamajna@gmail.com>
2025-05-02 16:11:12 -0700

    pin diffusers

--- server/pipelines/img2imgSDTurbo.py
+++ server/pipelines/img2imgSDTurbo.py
@@ -4,10 +4,6 @@ from diffusers import (
 )
 import torch
 
-try:
-    import intel_extension_for_pytorch as ipex  # type: ignore
-except:
-    pass
 
 from config import Args
 from pydantic import BaseModel, Field
@@ -106,35 +102,9 @@ class Pipeline:
         if args.pruna:
             # Create and smash your model
             smash_config = SmashConfig()
-            smash_config["cacher"] = "deepcache"
+            # smash_config["cacher"] = "deepcache"
             smash_config["compiler"] = "stable_fast"
             self.pipe = smash(model=self.pipe, smash_config=smash_config)
-
-        if args.sfast:
-            from sfast.compilers.stable_diffusion_pipeline_compiler import (
-                compile,
-                CompilationConfig,
-            )
-
-            print("\nRunning sfast compile\n")
-            from sfast.compilers.stable_diffusion_pipeline_compiler import (
-                compile,
-                CompilationConfig,
-            )
-
-            config = CompilationConfig.Default()
-            config.enable_xformers = True
-            config.enable_triton = True
-            config.enable_cuda_graph = True
-            self.pipe = compile(self.pipe, config=config)
-
-        if args.onediff:
-            print("\nRunning onediff compile\n")
-            from onediff.infer_compiler import oneflow_compile
-
-            self.pipe.unet = oneflow_compile(self.pipe.unet)
-            self.pipe.vae.encoder = oneflow_compile(self.pipe.vae.encoder)
-            self.pipe.vae.decoder = oneflow_compile(self.pipe.vae.decoder)
 
         self.pipe.set_progress_bar_config(disable=True)
         self.pipe.to(device=device, dtype=torch_dtype)
